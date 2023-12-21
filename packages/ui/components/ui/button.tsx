068f672f61280423ed7385e8ca188fa53a159013
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2023-12-21 16:05:32 +0100

    Refactor Button component to use forwardRef

--- packages/ui/components/ui/button.tsx
+++ packages/ui/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import {forwardRef} from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
@@ -39,7 +39,7 @@ export interface ButtonProps
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
