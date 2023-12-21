29f29ffb69c5a036ae7d547a0dd5803ae8e1f594
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2023-12-21 05:34:19 +0100

    add shadcn

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+export default function Page() {
+  return (
+    <>
+      <h1>Web</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
