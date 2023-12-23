3f4190b5f94b01410e42ebb95772c566d2e886b1
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2023-12-23 10:26:21 +0100

    fix: 🐛 content-script ui

--- apps/browser-extension/src/contents/google-sidebar.tsx
+++ apps/browser-extension/src/contents/google-sidebar.tsx
@@ -1,8 +1,11 @@
 import cssText from "data-text:./google-sidebar.css";
+import shadcnCssText from "data-text:../../../../packages/ui/styles/globals.css";
 import type { PlasmoCSConfig } from "plasmo";
 import { useEffect, useState } from "react";
 // Inject to the webpage itself
 import "./google-sidebar-base.css";
+
+import { Button } from "@repo/ui/components/ui/button";
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.google.com/*"],
@@ -11,7 +14,7 @@ export const config: PlasmoCSConfig = {
 // Inject into the ShadowDOM
 export const getStyle = () => {
   const style = document.createElement("style");
-  style.textContent = cssText;
+  style.textContent = cssText + shadcnCssText;
   return style;
 };
 
@@ -26,10 +29,15 @@ const GoogleSidebar = () => {
 
   return (
     <div id="sidebar" className={isOpen ? "open" : "closed"}>
-      <button className="sidebar-toggle" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? "🟡 Close" : "🟣 Open"}
-      </button>
-      <p>The Easiest Way to Build, Test, and Ship browser extensions</p>
+      <div id="root">
+        <button className="sidebar-toggle" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? "🟡 Close" : "🟣 Open"}
+        </button>
+        <div className="flex flex-col px-12 w-full justify-center">
+          <p>The Easiest Way to Build, Test, and Ship browser extensions</p>
+          <Button>Goooe</Button>
+        </div>
+      </div>
     </div>
   );
 };
--- apps/browser-extension/tailwind.config.js
+++ apps/browser-extension/tailwind.config.js
@@ -2,5 +2,7 @@ const ui = require("@repo/ui/tailwind.config");
 
 module.exports = {
   ...ui,
+  mode: "jit",
+  darkMode: "class",
   content: ["./src/**/*.tsx", "../../packages/ui/**/*.tsx"],
 };
