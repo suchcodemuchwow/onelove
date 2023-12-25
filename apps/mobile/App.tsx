6ee1c69d304eb104c9593efcfcd8f2a1444ecd5c
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2023-12-25 16:16:51 +0100

    style: 💄 formatting

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>
-        Hello World! 👋
-      </Text>
+      <Text>Hello World! 👋</Text>
     </View>
   );
 }
@@ -15,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,10 +1,10 @@
 // Learn more https://docs.expo.dev/guides/monorepos
-const { getDefaultConfig } = require('expo/metro-config');
-const { FileStore } = require('metro-cache');
-const path = require('path');
+const { getDefaultConfig } = require("expo/metro-config");
+const { FileStore } = require("metro-cache");
+const path = require("path");
 
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+const workspaceRoot = path.resolve(projectRoot, "../..");
 
 const config = getDefaultConfig(projectRoot);
 
@@ -14,13 +14,15 @@ config.watchFolders = [workspaceRoot];
 config.resolver.disableHierarchicalLookup = true;
 // #2 - Try resolving with project modules first, then workspace modules
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
 ];
 
 // Use turborepo to restore the cache when possible
 config.cacheStores = [
-  new FileStore({ root: path.join(projectRoot, 'node_modules', '.cache', 'metro') }),
+  new FileStore({
+    root: path.join(projectRoot, "node_modules", ".cache", "metro"),
+  }),
 ];
 
 module.exports = config;
